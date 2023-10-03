3d3d110c026d2a64a2cb89cdf7cc750dbcb5bd3a
dieum512 <akonkwadieum222@gmail.com>
2023-10-03 16:25:03 +0200

    Display the data titles

--- src/components/motor/Motor.jsx
+++ src/components/motor/Motor.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { motorsItem } from '../../redux/slice/motorSlice';
 import './motor.scss';
 
-const Motor = () => <div className="motor-container">Motor</div>;
+const Motor = () => {
+    const dispatch = useDispatch();
+    const {motorData} = useSelector((state) => state.motor)
+    useEffect(() => {
+        dispatch(motorsItem());
+    }, [dispatch])
+
+    return (
+        <>
+            {motorData.map((item) => (
+                <h1>{item.brand_name}</h1>
+            ))}
+        </>
+    );
+}
 
 export default Motor;
