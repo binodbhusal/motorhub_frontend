c76a5eee7f2861fb857c4d68e01f8360ce748dc8
dieum512 <akonkwadieum222@gmail.com>
2023-10-03 23:51:24 +0200

    Update the motors wit the swiper

--- src/components/motor/Motor.jsx
+++ src/components/motor/Motor.jsx
@@ -1,7 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { motorsItem } from '../../redux/slice/motorSlice';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {Pagination, Navigation} from 'swiper/modules'
 import './motor.css';
+
+// import Swiper core and required modules
+// import { Navigation } from 'swiper/modules';
+
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Motor = () => {
     const dispatch = useDispatch();
@@ -16,22 +28,35 @@ const Motor = () => {
                 <h1>Latest models</h1>
                 <h5>Please select a model</h5>
             </div>
-                <div className="motor-cards-cont">
+            {/* navigation={true} modules={[Navigation]} className="mySwiper" */}
+            <div className="swiper-cont">
+            <Swiper
+                    navigation={true}
+                    modules={[Navigation, Pagination]}
+                    className="mySwiper"
+                loopFillGroupWithBlank={true}
+                slidesPerView={3}
+                spaceBetween={20}
+                slidesPerGroup={1}
+            >
+                
                 {motorData.map((item) => (
-                    <div className="single-motor-cont">
-                        <img src={item.photo} alt={ item.brand_name } />
+                    <SwiperSlide>
+                        <img src={item.photo} alt={ item.brand_name } className='img-img'/>
                         <h4>
                             <span>{item.brand_name }</span>{" "}
                             <span>{ item.model_no }</span>
                         </h4>
-                        <p>{item.description}</p>
+                        <p className='item-description'>{item.description}</p>
                         <div className="social-links">
                             <i class="fa-brands fa-facebook-f"></i>
                             <i class="fa-brands fa-twitter"></i>
                             <i class="fa-brands fa-instagram"></i>
                         </div>
-                    </div>
+                    </SwiperSlide>
                 ))}
+                
+            </Swiper>
             </div>
         </div>
     );
