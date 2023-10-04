c578d124366fe004d759d06e602c015a76da7d6b
dieum512 <akonkwadieum222@gmail.com>
2023-10-04 13:16:23 +0200

    Add the details link

--- src/components/motor/Motor.jsx
+++ src/components/motor/Motor.jsx
@@ -2,13 +2,10 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { motorsItem } from '../../redux/slice/motorSlice';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination, Navigation} from 'swiper/modules'
+import { Pagination, Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 import './motor.css';
 
-// import Swiper core and required modules
-// import { Navigation } from 'swiper/modules';
-
-// import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -28,7 +25,6 @@ const Motor = () => {
                 <h1>Latest models</h1>
                 <h5>Please select a model</h5>
             </div>
-            {/* navigation={true} modules={[Navigation]} className="mySwiper" */}
             <div className="swiper-cont">
             <Swiper
                     navigation={true}
@@ -42,11 +38,13 @@ const Motor = () => {
                 
                 {motorData.map((item) => (
                     <SwiperSlide>
-                        <img src={item.photo} alt={ item.brand_name } className='img-img'/>
+                        <img src={item.photo} alt={item.brand_name} className='img-img' />
+                        <Link to={`/MotorDetails/${item.id}`}>
                         <h4>
                             <span>{item.brand_name }</span>{" "}
                             <span>{ item.model_no }</span>
                         </h4>
+                        </Link>
                         <p className='item-description'>{item.description}</p>
                         <div className="social-links">
                             <i class="fa-brands fa-facebook-f"></i>
