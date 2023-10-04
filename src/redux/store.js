47685b6e7d889641e9ea6586509d93e9cae42bd7
dieum512 <akonkwadieum222@gmail.com>
2023-10-04 13:18:25 +0200

    Add the data feched to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import motorReducer from './slice/motorSlice';
+import motorDetailReducer from './slice/motorDetailSlice';
 
 const store = configureStore({
     reducer: {
         motor: motorReducer,
+        motorDetails: motorDetailReducer,
     }
 })
 
