caa21bc8be0a7be7cc0de51a41aa0de5c6284267
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-10-06 10:40:49 +0500

    Add delete motor coponenet and delete fuctionalty

--- src/components/DeleteMotor/DeleteMotor.jsx
+++ src/components/DeleteMotor/DeleteMotor.jsx
@@ -1,5 +1,82 @@
-import React from 'react';
+import React from "react";
+import NavigationPanel from "../navigation/NavigationPanel";
+import MobileNavbar from "../navigation/MobileNavbar";
+import "./DeleteMotor.scss";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
+import { motorsItem, deleteMotorItem } from "../../redux/slice/motorSlice"; // Import the deleteMotorItem action
 
-const DeleteMotor = () => <div>DeleteMotor</div>;
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const DeleteMotor = () => {
+  const dispatch = useDispatch();
+  const { motorData } = useSelector((state) => state.motor);
+
+  useEffect(() => {
+    dispatch(motorsItem());
+  }, [dispatch]);
+
+  // Function to handle the deletion of a motor item
+  const handleDelete = (motor_id) => {
+    // Dispatch the deleteMotorItem action with the motorId
+    dispatch(deleteMotorItem(motor_id));
+  };
+
+  return (
+    <div className="main-container">
+      <MobileNavbar className="mobile-navbar" />
+      <NavigationPanel className="left-panel" />
+      <div className="motor-container">
+        <div className="models-titles">
+          <h1>Latest models</h1>
+          <h5>Please select a model</h5>
+        </div>
+        <div className="swiper-cont">
+          <Swiper
+            navigation
+            modules={[Navigation, Pagination]}
+            className="mySwiper"
+            loopFillGroupWithBlank
+            slidesPerView={3}
+            spaceBetween={20}
+            slidesPerGroup={1}
+          >
+            {motorData.map((item) => (
+              <SwiperSlide key={item.id}>
+                <img
+                  src={item.photo}
+                  alt={item.brand_name}
+                  className="img-img"
+                />
+                <Link to={`/MotorDetails/${item.id}`}>
+                  <h4>
+                    <span>{item.brand_name}</span> <span>{item.model_no}</span>
+                  </h4>
+                </Link>
+                <div className="social-links">
+                  <i className="fa-brands fa-facebook-f" />
+                  <i className="fa-brands fa-twitter" />
+                  <i className="fa-brands fa-instagram" />
+                </div>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => handleDelete(item.id)} // Call handleDelete with motor ID
+                >
+                  Delete Motor
+                </button>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DeleteMotor;
--- src/redux/slice/motorSlice.js
+++ src/redux/slice/motorSlice.js
@@ -1,36 +1,68 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const MOTORS_LINK = 'http://localhost:3000/api/motors/index';
+const MOTORS_LINK = "http://localhost:3000/api/motors/index";
+const DELETE_LINK = "http://localhost:3000/api/motors";
 
-export const motorsItem = createAsyncThunk('motor', async () => {
-  const response = await axios.get(MOTORS_LINK);
-  return response.data;
+export const motorsItem = createAsyncThunk("motor", async () => {
+  try {
+    const response = await axios.get(MOTORS_LINK);
+    return response.data;
+  } catch (error) {
+    throw error.message;
+  }
 });
 
+export const deleteMotorItem = createAsyncThunk(
+  "motor/deleteMotorItem",
+  async (motor_id) => {
+    try {
+      const response = await axios.delete(`${DELETE_LINK}/${motor_id}`);
+      return response.data;
+    } catch (error) {
+      throw error.message;
+    }
+  }
+);
 const initialState = {
   motorData: [],
+  motor_id: null, // Initialize motor_id to null
+  loading: false,
+  error: null,
 };
 
 const motorSlice = createSlice({
-  name: 'motors',
+  name: "motors",
   initialState,
-  // reducers: {},
+  // ... other code ...
+
   extraReducers: (builder) => {
     builder
-      .addCase(motorsItem.pending, (state) => ({
-        ...state,
-        loading: true,
-      }))
-      .addCase(motorsItem.fulfilled, (state, action) => ({
-        ...state,
-        loading: false, // Update loading to false
-        motorData: action.payload,
-      }))
-      .addCase(motorsItem.rejected, (state) => ({
-        ...state,
-        loading: false, // Update loading to false
-      }));
+      .addCase(deleteMotorItem.fulfilled, (state, action) => {
+        const motorIdToDelete = action.meta.arg;
+        state.motorData = state.motorData.filter(
+          (motor) => motor.id !== motorIdToDelete
+        );
+        state.loading = false;
+        state.error = null;
+        state.motor_id = null; // Reset motor_id after deletion
+      })
+      .addCase(motorsItem.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+        state.motor_id = null; // Reset motor_id when fetching motors
+      })
+      .addCase(motorsItem.fulfilled, (state, action) => {
+        state.loading = false;
+        state.motorData = action.payload;
+        state.error = null;
+      })
+      .addCase(motorsItem.rejected, (state) => {
+        state.loading = false;
+        state.error = "Failed to fetch motor data";
+      });
+
+    // Add other reducers and cases as needed...
   },
 });
 
