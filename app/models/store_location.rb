2cfb45555fee73eb0f5374cba58c9b606bd4b6b9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-10-07 15:24:25 +0100

    Added uniqueness to city name StoreLocation

--- app/models/store_location.rb
+++ app/models/store_location.rb
@@ -1,5 +1,5 @@
 class StoreLocation < ApplicationRecord
   has_many :motors, foreign_key: :location_id, dependent: :destroy
 
-  validates :city_name, presence: true
+  validates :city_name, presence: true, uniqueness: true
 end
