79b8fe3556e5041e00aa20a16cfe89b19bbef032
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-10-07 15:25:25 +0100

    Added Store location seed data and others data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ user2 = User.create(name: 'User 2', role:'admin')
 
 # Seed data for StoreLocation table
 location1 = StoreLocation.create(city_name: 'Lisbon')
+location2 = StoreLocation.create(city_name: 'Kathmandu')
+
 
 # Seed data for Motor table
 motor1 = Motor.create(
@@ -12,7 +14,7 @@ motor1 = Motor.create(
   brand_name: 'Porsche',
   model_no: '718',
   manufacturer: 'Porsche Automobil Holding SE',
-  manufacturer_date: Date.new(2022, 1, 15),
+  manufacture_date: Date.new(2022, 1, 15),
   description: 'These models were made for the sport of it. They are mid-engined roadsters that unite the sporting spirit of the legendary Porsche 718 with the sports car of tomorrow – and transfer it to the roads of today’s world. With one goal: to take the everyday out of every day. What is it that makes the 718 models so special? Their performance, their handling, their design, their generous amount of space with two luggage compartments. But most of all: a mid-engined layout that makes all this possible in the first place.',
   photo: 'https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_1280.png',
   unit_price: 60000.0,
@@ -23,11 +25,11 @@ motor1 = Motor.create(
 
 motor2 = Motor.create(
   user_id: user2.id,
-  location_id: location1.id,
+  location_id: location2.id,
   brand_name: 'BMW',
   model_no: '456',
   manufacturer: 'Bayerische Motoren Werke AG',
-  manufacturer_date: Date.new(2023, 3, 20),
+  manufacture_date: Date.new(2023, 3, 20),
   description: 'THE SECOND GENERATION.
   (BMW F48)
   BMW X Series BMW X1 F48 second generation three-quarter side view standing in front of a city
@@ -45,7 +47,7 @@ motor3 = Motor.create(
   brand_name: 'Harley Davidson',
   model_no: '1918',
   manufacturer: 'Harley-Davidson, Inc.',
-  manufacturer_date: Date.new(2023, 5, 25),
+  manufacture_date: Date.new(2023, 5, 25),
   description: 'A liquid-cooled V-Twin tuned to make tremendous torque at low RPM, with a torque curve that stays flat through the powerband.Build road confidence by selecting different pre-programmed ride modes. Switch between Road, Rain and Sport, or customize your own.',
   photo: 'https://cdn.pixabay.com/photo/2017/01/11/15/26/harley-1972061_1280.jpg',
   unit_price: 14999.0,
@@ -61,7 +63,7 @@ motor4 = Motor.create(
   brand_name: 'Vespa GTS',
   model_no: '497',
   manufacturer: 'Piaggio & C. SpA',
-  manufacturer_date: Date.new(2023, 5, 25),
+  manufacture_date: Date.new(2023, 5, 25),
   description: 'The GTS chassis has improved suspension too. While both front and rear units have been retuned to further improve comfort and stability, the front suspension in particular has been vastly redesigned. While still based on the traditional trailing-link layout, the components now appear stronger and show more refined geometry, including the brake caliper mounting. Piaggio did not release any specific technical data, but it claims that the front end now feels more stable at speed and more responsive as well.',
   photo: 'https://www.algateckids.pt/images/articulos/max/moto_vespa_classica_metallica_ambosstoys_3202-1.jpg',
   unit_price: 7799.0,
@@ -76,7 +78,7 @@ motor5 = Motor.create(
   brand_name: 'Tesla',
   model_no: '1020',
   manufacturer: 'Tesla Motors',
-  manufacturer_date: Date.new(2023, 1, 20),
+  manufacture_date: Date.new(2023, 1, 20),
   description: 'The car that kickstarted the EV revolution is starting to show its age. While the 2023 Tesla Model S falls behind newly developed rivals with more over-the-top features and longer driving ranges, the sedan-like hatchback continues to be competitive. After all, the Model S offers up to 405 miles of range on a single charge. Plus, the 1020-hp tri-motor Plaid performance model leaps to 60 mph in just 2.1 seconds, which is as quick as a multimillion-dollar hypercar.',
   photo: 'https://hips.hearstapps.com/hmg-prod/images/2020-porsche-taycan-4s-vs-2020-tesla-model-s-long-range-202-1621386342.jpg?crop=0.842xw:0.711xh;0,0.161xh&resize=1200:*',
   unit_price: 76630.0,
