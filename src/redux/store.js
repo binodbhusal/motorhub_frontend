e00f195c1ce37f9a0085352764ab0bdeb0bd54d4
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-10-07 23:11:50 +0100

    add cityname and reserve Reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import motorReducer from './slice/motorSlice';
 import motorDetailReducer from './slice/motorDetailSlice';
 import addMotorsReducer from './slice/addMotorSlice';
+import reserveReducer from './slice/reservationSlice';
+import citynameReducer from './slice/citySlice';
 
 const store = configureStore({
   reducer: {
     motor: motorReducer,
     motorDetails: motorDetailReducer,
     addNewMotor: addMotorsReducer,
+    cityname: citynameReducer,
+    reserve: reserveReducer,
   },
 });
 
