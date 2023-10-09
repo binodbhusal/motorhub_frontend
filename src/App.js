cfe4470d256037e03d095c5badcb5a9802bf8ea4
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-10-09 16:06:42 +0200

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import MainPage from './components/mainpage/MainPage';
 import Reserve from './components/Reserve/Reserve';
 import MyReservations from './components/MyReservations/MyReservations';
@@ -7,18 +8,30 @@ import AddMotor from './components/AddMotor/AddMotor';
 import DeleteMotor from './components/DeleteMotor/DeleteMotor';
 import NoMatch from './NoMatch/NoMatch';
 import MotorDetails from './components/details/MotorDetails';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
+  const user = useSelector((store) => store.user);
   return (
     <div className="app-container">
       <Router>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/Reserve" element={<Reserve />} />
-          <Route path="/MyReservations" element={<MyReservations />} />
-          <Route path="/AddMotor" element={<AddMotor />} />
-          <Route path="/DeleteMotor" element={<DeleteMotor />} />
-          <Route path="/MotorDetails/:id" element={<MotorDetails />} />
+          {!user.logedIn ? (
+            <>
+              <Route path="login" element={<Login />} />
+              <Route path="sign_up" element={<SignUp />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/Reserve" element={<Reserve />} />
+              <Route path="/MyReservations" element={<MyReservations />} />
+              <Route path="/AddMotor" element={<AddMotor />} />
+              <Route path="/DeleteMotor" element={<DeleteMotor />} />
+              <Route path="/MotorDetails/:id" element={<MotorDetails />} />
+            </>
+          )}
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </Router>
