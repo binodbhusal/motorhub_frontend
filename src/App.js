da44f9d68544476fbfcebc3ac88b17d1ebee2ded
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-10-10 18:12:04 +0200

    Protect routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import MainPage from './components/mainpage/MainPage';
 import Reserve from './components/Reserve/Reserve';
 import MyReservations from './components/MyReservations/MyReservations';
@@ -10,28 +9,23 @@ import NoMatch from './NoMatch/NoMatch';
 import MotorDetails from './components/details/MotorDetails';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import PrivateRoutes from './PrivateRoutes/PrivateRoutes';
 
 function App() {
-  const user = useSelector((store) => store.user);
   return (
     <div className="app-container">
       <Router>
         <Routes>
-          {!user.logedIn ? (
-            <>
-              <Route path="login" element={<Login />} />
-              <Route path="sign_up" element={<SignUp />} />
-            </>
-          ) : (
-            <>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/Reserve" element={<Reserve />} />
-              <Route path="/MyReservations" element={<MyReservations />} />
-              <Route path="/AddMotor" element={<AddMotor />} />
-              <Route path="/DeleteMotor" element={<DeleteMotor />} />
-              <Route path="/MotorDetails/:id" element={<MotorDetails />} />
-            </>
-          )}
+          <Route path="login" element={<Login />} />
+          <Route path="sign_up" element={<SignUp />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/Reserve" element={<Reserve />} />
+            <Route path="/MyReservations" element={<MyReservations />} />
+            <Route path="/AddMotor" element={<AddMotor />} />
+            <Route path="/DeleteMotor" element={<DeleteMotor />} />
+            <Route path="/MotorDetails/:id" element={<MotorDetails />} />
+          </Route>
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </Router>
