6ddeb9192f1436f6cbe742f41724513a22141454
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-11 19:54:35 +0000

    update user registration swagger spec

--- spec/requests/users/registrations_spec.rb
+++ spec/requests/users/registrations_spec.rb
@@ -1,41 +1,57 @@
-# registration_spec.rb
+# spec/requests/user_registration_spec.rb
 
 require 'swagger_helper'
 
-
 RSpec.describe 'User Registration', type: :request do
-  it 'registers a user' do
-      user_attributes = {
-      email: 'user@example.com',
-      password: 'password',
-      password_confirmation: 'password',
-      name: 'John'
-    }
-
-    post '/users', params: { user: user_attributes }, as: :json
-
-    expect(response).to have_http_status(:ok)
-
-    # Verify that the user is created in the database
-    user = User.find_by(email: user_attributes[:email])
-    expect(user).not_to be_nil
-
+  before(:each) do
+    create_list(:user, 1) # Create some users before running the test
   end
-  it 'handles invalid user registration' do
-    invalid_user_attributes = {
-      email: 'invalid-email',
-      password: 'short',
-      password_confirmation: 'mismatch',
-      name: 'Invalid User'
-    }
 
-    post '/users', params: { user: invalid_user_attributes }, as: :json
+  path '/users' do
+    post 'Registers a user' do
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          email: { type: :string },
+          password: { type: :string },
+          password_confirmation: { type: :string }
+        },
+        required: %w[name email password password_confirmation]
+      }
 
-    expect(response).to have_http_status(:unprocessable_entity)
+      response '200', 'user registered' do
+        let(:user_attributes) do
+          attributes_for(:user) # Use FactoryBot to generate user attributes
+        end
 
-    # Verify that the user is not created in the database
-    user = User.find_by(email: invalid_user_attributes[:email])
-    expect(user).to be_nil
+        let(:user) { { user: user_attributes } } # Add this line
 
+        run_test! do
+          post '/users', params: { user: user_attributes }, as: :json
+          created_user = User.find_by(email: user_attributes[:email])
+          expect(created_user).not_to be_nil
+        end
+      end
+
+      response '422', 'invalid user registration' do
+        let(:invalid_user_attributes) do
+          attributes_for(:user, email: 'invalid-email', password: 'short', password_confirmation: 'mismatch',
+                                name: 'Invalid User')
+        end
+
+        let(:user) { { user: invalid_user_attributes } } # Add this line
+
+        run_test! do
+          post '/users', params: { user: invalid_user_attributes }, as: :json
+          expect(response).to have_http_status(:unprocessable_entity)
+
+          # Verify that the user is not created in the database
+          user = User.find_by(email: invalid_user_attributes[:email])
+          expect(user).to be_nil
+        end
+      end
+    end
   end
 end
