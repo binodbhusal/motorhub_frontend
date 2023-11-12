d06d1d85334a0f12e56dda6281b10e11272bef8c
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-12 22:06:53 +0000

    Add motor delete test case

--- spec/requests/api/motors_spec.rb
+++ spec/requests/api/motors_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe 'API Motors', type: :request do
   path '/api/motors' do
     before(:all) do
       @user = create(:user) # Create a user using the factory
-      @store_location = create(:store_location) # Create a StoreLocation using the factory
+      @store_location = create(:store_location) 
+
+
     end
     get 'Lists all motors' do
       tags 'Motors'
       produces 'application/json'
-      response '200', 'list of motors' do
+      response '200', 'List of motors - Successful' do
         run_test! do
           expect(response).to have_http_status(:ok)
         end
@@ -39,7 +41,7 @@ RSpec.describe 'API Motors', type: :request do
         required: %w[brand_name model_no manufacturer manufacture_date description photo
                      unit_price purchase_fee finance_fee total_price user_id location_id]
       }
-      response '201', 'motor created' do
+      response '201', 'Motor created - Successful' do
         let(:motor) do
           {
             user_id: @user.id,
@@ -61,5 +63,35 @@ RSpec.describe 'API Motors', type: :request do
         end
       end
     end
+    
+    path '/api/motors/{id}' do
+      before do
+        @motor = create(:motor, user: @user)
+      end
+      parameter name: :id, in: :path, type: :integer, description: 'Motor ID'
+  
+      delete 'Deletes a motor' do
+        tags 'Motors'
+        produces 'application/json'
+  
+        response '204', 'Motor deleted - Successful' do
+          let(:id) { @motor.id }
+  
+          run_test! do
+            expect(response).to have_http_status(:no_content)
+          end
+        end
+  
+        response '404', 'Motor not found' do
+          let(:id) { '9999' } # Assuming 999 is an invalid motor ID
+  
+          run_test! do
+            expect(response).to have_http_status(:not_found)
+          end
+        end
+      end
+    end
+    
+
   end
 end
