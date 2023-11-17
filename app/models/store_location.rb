378882b2fcc75a8c7b4967cc4a796aa3c3a859ab
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-17 21:17:16 +0000

    clear main branch

--- app/models/store_location.rb
+++
@@ -1,5 +0,0 @@
-class StoreLocation < ApplicationRecord
-  has_many :motors, foreign_key: :location_id, dependent: :destroy
-
-  validates :city_name, presence: true, uniqueness: true
-end
--- spec/factories/stores_location.rb
+++
@@ -1,6 +0,0 @@
-# spec/factories/cities.rb
-FactoryBot.define do
-  factory :store_location do
-    city_name { Faker::Address.city }
-  end
-end
