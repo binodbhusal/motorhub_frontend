886e0bf05df08efa80f401f4bd354b28aff007c6
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-20 21:14:49 +0000

    Add private route and login, signup with proper auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/mainpage/MainPage';
 import MyReservations from './components/MyReservations/MyReservations';
@@ -8,26 +7,47 @@ import NoMatch from './NoMatch/NoMatch';
 import MotorDetails from './components/details/MotorDetails';
 import Login from './components/user/Login';
 import Signup from './components/user/Signup';
-import PrivateRoutes from './PrivateRoutes/PrivateRoutes';
 import Reservation from './components/Reserve/Reservation';
 import ReserveConfirm from './components/Reserve/ReserveConfirm';
+import PrivateRoute from './PrivateRoutes/PrivateRoutes';
 
 function App() {
   return (
     <div className="app-container">
       <Router>
         <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="SignUp" element={<Signup />} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<MainPage />} />
-          <Route path="/Reserve" element={<Reservation />} />
-          <Route path="/MyReservations" element={<MyReservations />} />
-          <Route path="/AddMotor" element={<AddMotor />} />
-          <Route path="/DeleteMotor" element={<DeleteMotor />} />
-          <Route path="/reserveform/:id" element={<Reservation />} />
-          <Route path="/reserveconfirm" element={<ReserveConfirm />} />
-          <Route path="/MotorDetails/:id" element={<MotorDetails />} />
+
+          <Route
+            path="/reserve"
+            element={<PrivateRoute element={<Reservation />} />}
+          />
+          <Route
+            path="/myreservations"
+            element={<PrivateRoute element={<MyReservations />} />}
+          />
+          <Route
+            path="/addmotor"
+            element={<PrivateRoute element={<AddMotor />} />}
+          />
+          <Route
+            path="/deletemotor"
+            element={<PrivateRoute element={<DeleteMotor />} />}
+          />
+          <Route
+            path="/reserveform/:id"
+            element={<PrivateRoute element={<Reservation />} />}
+          />
+          <Route
+            path="/reserveconfirm"
+            element={<PrivateRoute element={<ReserveConfirm />} />}
+          />
+          <Route
+            path="/motordetails/:id"
+            element={<PrivateRoute element={<MotorDetails />} />}
+          />
 
           <Route path="*" element={<NoMatch />} />
         </Routes>
