d7d7c4c4d3e16c93349d7b6b96c768f495f71cc9
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-04 11:15:07 +0000

    update logout component

--- src/components/user/Logout.js
+++ src/components/user/Logout.js
@@ -1,16 +1,23 @@
 import { useEffect, useState, useCallback } from 'react';
-
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { clearUser } from '../../redux/slice/userSlice';
 import { destroySession, TOKENKEY } from '../util/auth';
 
 const Log = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogout = useCallback(() => {
     setIsLoggedIn(false);
     destroySession();
+    dispatch(clearUser());
     window.location.reload();
-  }, []);
-
+  }, [dispatch]);
+  const handleLogin = () => {
+    navigate('/login');
+  };
   useEffect(() => {
     if (localStorage.getItem(TOKENKEY)) {
       setIsLoggedIn(true);
@@ -31,7 +38,15 @@ const Log = () => {
   }
 
   return (
-    <p>Please Login</p>
+    <button
+      type="button"
+      htmlFor="btnlogin"
+      onClick={handleLogin}
+    >
+      Please login
+      {' '}
+
+    </button>
   );
 };
 export default Log;
